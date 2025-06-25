d56c3e6933856a8164d6ef7aeef78719d596e555
yarik <super.racing@ukr.net>
2025-06-25 18:39:10 +0300

    finished tests

--- app/main.py
+++ app/main.py
@@ -12,15 +12,17 @@ def create_person_list(people: list) -> list:
     for index in range(0, len(people)):
         result.append(Person(people[index]["name"], people[index]["age"]))
     for index in range(0, len(people)):
-        if people[index]["wife"] is not None and not result[index].wife:
+        if "wife" in people[index] and people[index]["wife"] is not None and not hasattr(result[index], "wife"):
             result[index].wife = people[index]["wife"]
             for num in range(0, len(result)):
                 if result[num].name is result[index].wife:
-                    result[num].husband = result[index].name
-        elif (people[index]["husband"] is not None
-              and not result[index].husdand):
+                    result[num].husband = result[index]
+                    result[index].wife = result[num]
+        elif "husband" in people[index] and people[index]["husband"] is not None and \
+            not hasattr(result[index], "husband"):
             result[index].husband = people[index]["husband"]
             for num in range(0, len(result)):
                 if result[num].name is result[index].husband:
-                    result[num].wife = result[index].name
+                    result[num].wife = result[index]
+                    result[index].husband = result[num]
     return result
