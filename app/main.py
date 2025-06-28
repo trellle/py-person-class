9f51213b35357cb0391971c3f7327cbd0372a626
yarik <super.racing@ukr.net>
2025-06-28 21:28:59 +0300

    refactored my code

--- app/main.py
+++ app/main.py
@@ -4,27 +4,25 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        Person.people.update({self.name: self})
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    result = []
-    for index in range(0, len(people)):
-        result.append(Person(people[index]["name"], people[index]["age"]))
+    result = [Person(elem["name"], elem["age"]) for elem in people]
     for index in range(0, len(people)):
         if ("wife" in people[index] and people[index]["wife"] is not None
                 and not hasattr(result[index], "wife")):
             result[index].wife = people[index]["wife"]
-            for num in range(0, len(result)):
-                if result[num].name is result[index].wife:
-                    result[num].husband = result[index]
-                    result[index].wife = result[num]
+            for elem in result:
+                if elem.name is result[index].wife:
+                    elem.husband = result[index]
+                    result[index].wife = elem
         elif ("husband" in people[index]
               and people[index]["husband"] is not None
               and not hasattr(result[index], "husband")):
             result[index].husband = people[index]["husband"]
-            for num in range(0, len(result)):
-                if result[num].name is result[index].husband:
-                    result[num].wife = result[index]
-                    result[index].husband = result[num]
+            for elem in result:
+                if elem.name is result[index].husband:
+                    elem.wife = result[index]
+                    result[index].husband = elem
     return result
