c00d6341e40c91bb7f79e8f6d63e08ff304bd228
yarik <super.racing@ukr.net>
2025-07-17 17:56:59 +0300

    python style

--- app/main.py
+++ app/main.py
@@ -7,22 +7,13 @@ class Person:
         Person.people[self.name] = self
 
 
-def create_person_list(people: list) -> list:
-    result = []
-    for elem in people:
-        person = Person(elem["name"], elem["age"])
-        if elem.get("wife"):
-            person.wife = elem["wife"]
-        elif elem.get("husband"):
-            person.husband = elem["husband"]
-        result.append(person)
-        person = None
-    for elem1 in result:
-        for elem2 in result:
-            if hasattr(elem1, "wife") and elem1.wife is elem2.name:
-                elem2.husband = elem1
-                elem1.wife = elem2
-            elif hasattr(elem1, "husband") and elem1.husband is elem2.name:
-                elem2.wife = elem1
-                elem1.husband = elem2
-    return result
+def create_person_list(people_data: list) -> list:
+    people = [Person(person["name"], person["age"]) for person in people_data]
+    for index, person in enumerate(people_data):
+        wife = people_data[index].get("wife")
+        if wife:
+            people[index].wife = Person.people[wife]
+        husband = people_data[index].get("husband")
+        if husband:
+            people[index].husband = Person.people[husband]
+    return people
